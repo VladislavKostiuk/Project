8e69a414386f8d0501316c409fd4fc1440ec6215
Тимофей Красько <timakrasko2004@gmail.com>
2023-06-19 23:47:19 +0300

    Initial commit

---
+++ src/main/java/com/example/Project/SiteController.java
@@ -0,0 +1,12 @@
+package com.example.Project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SiteController {
+    @GetMapping("/main")
+    public String getMain(){
+        return "main";
+    }
+}
